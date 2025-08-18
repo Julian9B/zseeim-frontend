9973623ebaa5a32e2464406b071a4dcf9e960651
Julian <julian.broniewski@gmail.com>
2025-08-18 20:53:12 +0200

    Added working navbar display

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,20 @@
-//
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("scroll", (event) => { })
+
+})
+
+onscroll = (event) => {
+    let subNavbars = document.querySelectorAll(".subNavbar");
+
+    if(window.scrollY > 316) {
+        subNavbars.forEach(subNavbar => {
+            subNavbar.classList.remove("subNavbarTop");
+            subNavbar.classList.add("subNavbarBottom");
+        });
+    } else {
+        subNavbars.forEach(subNavbar => {
+            subNavbar.classList.remove("subNavbarBottom");
+            subNavbar.classList.add("subNavbarTop");
+        });
+    }
+}
