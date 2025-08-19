5a60bb6a442b0252757566d5ef54c006a7a6d491
Julian <julian.broniewski@gmail.com>
2025-08-19 16:31:49 +0200

    Minor navbar changes

--- frontend/main.js
+++ frontend/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 onscroll = (event) => {
     let subNavbars = document.querySelectorAll(".subNavbar");
 
-    if(window.scrollY > 316) {
+    if(window.scrollY > 325) {
         subNavbars.forEach(subNavbar => {
             subNavbar.classList.remove("subNavbarTop");
             subNavbar.classList.add("subNavbarBottom");
