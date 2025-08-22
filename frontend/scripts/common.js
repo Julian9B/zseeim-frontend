c2d6cc7467d9f5a01091f5020d5042c401e9cc6e
Julian <julian.broniewski@gmail.com>
2025-08-22 20:59:53 +0200

    Added more accessibility settings, minor cosmetic changes

--- frontend/scripts/common.js
+++ frontend/scripts/common.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const html = document.documentElement;
 
-
     // wielkość tekstu
 
     let fontSize = parseInt(localStorage.getItem("fontSize")) || 16;
@@ -36,25 +35,71 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-
     // skala szarości
 
     const toggleGrayscaleButton = document.querySelector(".toggleGrayscale");
 
     if (localStorage.getItem("grayscale") === "on") {
-        html.classList.add("grayscale");
+        toggleGrayscale();
         console.log(localStorage.getItem("grayscale"));
     }
 
     toggleGrayscaleButton.addEventListener("click", () => {
-        html.classList.toggle("grayscale");
+        toggleGrayscale();
 
         if (html.classList.contains("grayscale")) {
             localStorage.setItem("grayscale", "on");
-            console.log("Grayscale on");
         } else {
             localStorage.setItem("grayscale", "off");
-            console.log("Grayscale off");
         }
     });
+
+    function toggleGrayscale() {
+        html.classList.toggle("grayscale");
+    }
+
+    // wysoki kontrast
+
+    const toggleContrastButton = document.querySelector(".toggleContrast");
+    const accessibility = document.querySelector("header .accessibility")
+    const accessibilityChildren = accessibility.querySelectorAll('*');
+    const navbar = document.querySelector(".navbar")
+    const navbarChildren = navbar.querySelectorAll('*');
+    const main = document.querySelector("main")
+    const mainChildren = main.querySelectorAll('*');
+    const footer = document.querySelector("footer")
+    const footerChildren = footer.querySelectorAll('*');
+
+    if (localStorage.getItem("contrast") === "on") {
+        toggleContrast();
+    }
+
+    toggleContrastButton.addEventListener("click", () => {
+        toggleContrast();
+
+        if (accessibility.classList.contains("contrastWhite")) {
+            localStorage.setItem("contrast", "on");
+        } else {
+            localStorage.setItem("contrast", "off");
+        }
+    });
+
+    function toggleContrast() {
+        accessibility.classList.toggle("contrastWhite");
+        accessibilityChildren.forEach(child => {
+            child.classList.toggle("contrastWhite");
+        });
+        navbar.classList.toggle("contrastYellow");
+        navbarChildren.forEach(child => {
+            child.classList.toggle("contrastYellow");
+        });
+        main.classList.toggle("contrastWhite");
+        mainChildren.forEach(child => {
+            child.classList.toggle("contrastWhite");
+        });
+        footer.classList.toggle("contrastWhite");
+        footerChildren.forEach(child => {
+            child.classList.toggle("contrastWhite");
+        });
+    }
 });
